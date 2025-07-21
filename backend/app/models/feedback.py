87aa5ad17e07bd6683d1183e5bfd1b01880731df
Gulshan <gulshan.kumar.dsa@gmail.com>
2025-07-21 22:25:28 +0300

    Create feedback table

---
+++ backend/app/models/feedback.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from datetime import datetime
+from core.database import Base
+
+class Feedback(Base):
+    __tablename__ = "feedback"
+
+    id = Column(Integer, primary_key=True, index=True)
+    customer_name = Column(String(100))
+    message = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
